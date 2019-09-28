eb43b0adeecea457cbce42187793f160476acc21
arqdsis <arqdsis@MOOLIi0317.animaedu.intranet>
2019-09-28 15:57:21 -0300

    ionic notas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  notes = [
+    {
+    id: '1',
+    date: '2016-02-01',
+    title: 'Firebase',
+    content: 'Que tal programar Serverless?'
+    },
+    {
+    id: '2',
+    date: '2016-01-01',
+    title: 'Ionic',
+    content: 'Aprenda o básico de Ionic'
+    },
+    {
+    id: '3',
+    date: '2016-03-01',
+    title: 'Angular',
+    content: 'Importante para desenvolver com Ionic'
+    }
+    ]
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onItemClick(note){
+    console.log('item.click', note);
   }
 
 }
